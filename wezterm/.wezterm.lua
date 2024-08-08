036ab7afe8f13d15d91a9c4665d0a9afd2e31b5e
garza <garza@cjas.org>
2024-08-08 10:11:26 -0500

    adding wezterm

---
+++ wezterm/.wezterm.lua
@@ -0,0 +1,14 @@
+local wezterm = require 'wezterm'
+local config = {}
+
+config.font = wezterm.font 'ComicMono Nerd Font Mono'
+config.font_size = 18.0
+config.color_scheme = "Catppuccin Mocha"
+-- , -- or Macchiato, Frappe, Latte
+config.window_background_opacity = 0.7
+config.text_background_opacity = 1.0
+
+config.enable_tab_bar = false
+config.window_decorations = "RESIZE"
+
+return config
